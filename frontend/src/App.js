f0bb883709d8b6636275734cb8bce8e1418c6495
Narcisa Hadžajlić <75080129+NarcisaH@users.noreply.github.com>
2026-01-05 14:41:52 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Travelers from './Travelers';
+//import Agencies from './Agencies';
+//import Trips from './Trips';
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          {/* Početna ruta */}
+          <Route path="/" element={<Home />} />
+
+          {/* Ostale rute */}
+          <Route path="/travelers" element={<Travelers />} />
+            {/*<Route path="/agencies" element={<Agencies />} />
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
+
